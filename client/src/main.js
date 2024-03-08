bbeb077db9f8e5bc192d8051e63f73cc456d0663
yamoo9 <yamoo9@naver.com>
2024-03-08 13:50:53 +0900

    [구성] VS Code & Proxy
    - VS Code 설정
    - Proxy 서버 오류 메세지 추가

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,14 @@ import './styles/globals.css';
 
 async function main() {
   const response = await fetch('/api/posts?page=1');
-  const posts = await response.json();
-  console.log(posts);
+  if (response.ok) {
+    const posts = await response.json();
+    console.log(posts);
+  } else {
+    console.error(
+      'Proxy (개발) 서버에 문제가 발생했습니다.\n  서버가 구동되었는지 확인하세요.'
+    );
+  }
 }
 
 main();
